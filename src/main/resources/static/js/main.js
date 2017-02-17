d90fa1e74dfcf319f563c76911b72fef05d6c03c
alinaberlin <alina.ghetler@gmail.com>
2017-02-17 17:38:07 +0100

    Draft user list

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,9 +7,23 @@ $("#register").submit(function(event){
     $.ajax({
         url : post_url,
         type: request_method,
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         data : form_data
-    }).done(function(response) {
-        alert("Ok");
+    }).done(function(data) {
+        listUsers();
         //$("#server-results").html(response);
+    }).fail(function(error) {
+        alert("Password confirmation don't match!");
     });
 });
+
+function listUsers() {
+    $.ajax({
+        url : "api/user/AllUsers?page=0&size=20",
+        type: "get"
+    }).done(function(data) {
+        var tmpl = $.templates("#userListTemplate");
+        $("#main").html(tmpl.render(data));
+        //$("#server-results").html(response);
+    })
+}
